155ca8b29af47c930985d761e4b7902ff9718315
박세원 <bttb66@bagsewon-ui-MacBook-Air.local>
2017-09-10 21:13:57 +0900

    redirect

--- content.js
+++ content.js
@@ -5,7 +5,11 @@ var searchByRepo = document.getElementById("repo");
 document.getElementById("search").addEventListener("click", function() {
     var keyword = searchText.value;
     if (searchByUser.checked) {
-        chrome.tabs.create({ url : "https://github.com/" + keyword});
+        //user검색 선택
+        chrome.tabs.create({ url : "https://github.com/" + keyword},(tab) =>{
+            if (tab.title == 'Page not found &middot; GitHub')
+                chrome.tabs.update(tab.id, { url: "https://github.com/search?utf8=%E2%9C%93&q=" + keyword + "/"});
+        });
     } else {
         chrome.tabs.create({ url : "https://github.com/search?utf8=%E2%9C%93&q=" + keyword +"/"});
     }
